bc0c0b7dc8122ad0e5366b5f1567ad11dcb2879c
Azim071 <azimdzalalbekov913@gmail.com>
2024-09-26 20:56:13 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,86 @@
+// const btn = document.getElementById ('btn')
+// const root = document.getElementById('root')
+// btn.onclick = () => {
+// const newContent = document.createElement('p') 
+// const del = document.createElement('button')
+
+// newContent.textContent = 'New text'
+// del.textContent = "Delete"
+
+// newContent.appendChild(del)
+
+// root.appendChild(newContent)
+
+// del.onclick = () => {
+//     root.removeChild(newContent)
+// }
+// }
+
+// const counter = document.querySelector ('#count')
+// const btns = document.querySelectorAll("button")
+
+
+
+// let count = 0
+
+// btns[0].onclick = () => {
+    
+//     count++
+     
+
+//     counter.innerText = count
+//     counter.style.color = 'red'
+// }
+
+
+
+
+// btns[1].onclick = () => {
+    
+//     if (count > 0) {
+//         count--
+//     } 
+//     counter.innerText = count
+// }
+
+
+
+
+// btns[2].onclick = () => {
+ 
+//     counter.innerText = count = 0
+// }
+
+
+// const btns = document.querySelectorAll('button')
+// const body = document.querySelector('body')
+
+
+
+// btns[0].onclick = () => {
+//     body.style.backgroundColor = "red"
+
+// }
+
+// btns[1].onclick = () => {
+//     body.style.backgroundColor='blue'
+// }
+
+
+// btns[2].onclick = () => {
+//     body.style.backgroundColor='green'
+// }
+// btns[3].onclick = () => {
+//     body.style.backgroundColor='yellow'
+// }
+
+
+// const h1 = document.querySelector('h1')
+// const input = document.querySelector('input')
+// const btn = document.querySelector('button')
+
+// // h1.innerHTML = '<del>Azim</del>'
+
+// btn.onclick = () => {
+//     btn.innerHTML = input.value
+// }    
